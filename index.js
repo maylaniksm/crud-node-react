4e5fc515f7f907de7cd22b57c0fa425324516cbd
maylaniksm <63834571+maylaniksm@users.noreply.github.com>
2021-03-18 15:11:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const auth = require("./auth")
 const user = require("./route/user")
 app.use("/", user)
 
+//import route jurusan
+const jurusan = require("./route/jurusan")
+app.use("/jurusan", auth, jurusan)
+
 //import route pegawai
 const pegawai = require("./route/pegawai")
 app.use("/pegawai", auth, pegawai)
@@ -20,6 +24,15 @@ app.use("/pelanggaran", auth, pelanggaran)
 //import route siswa
 const siswa = require("./route/siswa")
 app.use("/siswa", auth, siswa)
+
+//base url -> http://localhost:2000/pelanggaran_siswa
+const pelanggaran_siswa = require("./route/pelanggaran_siswa")
+app.use("/pelanggaran_siswa", auth, pelanggaran_siswa)
+
+//base url -> http://localhost:2000/detail_pelanggaran_siswa
+const detail_pelanggaran_siswa = require("./route/detail_pelanggaran_siswa")
+app.use("/detail_pelanggaran_siswa", auth, detail_pelanggaran_siswa)
+
 
 //membuat web server dengan port 2000
 app.listen(2000, () => {
